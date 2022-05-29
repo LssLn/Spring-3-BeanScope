d94d5323d57fe75839d0ba31f5264529a09e9a28
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-05-29 20:01:25 +0200

    Add files via upload

---
+++ 3 BeanLifeCycle PostConstruct/src/main/java/com/example/demo/scope/PersonDAO.java
@@ -0,0 +1,32 @@
+package com.example.demo.scope;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class PersonDAO {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	JdbcConnection jdbcConnection;
+
+	public JdbcConnection getJdbcConnection() {
+		return jdbcConnection;
+	}
+
+	public void setJdbcConnection(JdbcConnection jdbcConnection) {
+		this.jdbcConnection = jdbcConnection;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+}
